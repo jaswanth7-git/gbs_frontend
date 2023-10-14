d91035e65cc9b646f142c46f3ae90c52fd2e4390
krishnadheerajp <krishna.dheeraj@bajajfinserv.in>
2023-10-14 19:22:52 +0530

    advance and scheme api integration done

--- src/app/components/advance/advance.component.ts
+++ src/app/components/advance/advance.component.ts
@@ -15,24 +15,16 @@ alladvances:any;
 constructor(private api: ApicallService) {}
 
 ngOnInit() {
-  this.api.getApi("api/sales/").subscribe((response) => {
-    // this.alladvances = response;
+  this.api.getApi("api/advance/").subscribe((response) => {
+    this.alladvances = response;
   });
-  //  Mocking the values since API not completed yet
-  this.alladvances=`[
-    {"Date": 1,"Description": "delectus aut autem","Amount": 12345},
-    {"Date": 2,"Description": "lorem","Amount": 2393209}
-  ]`;
-  this.alladvances=JSON.parse(this.alladvances);
-  console.log(this.alladvances)
 }
 add_advance(mobileno:any){
     let data = {
       Amount: this.advanceamt
     };
-    this.api.postApi("api/sales/"+mobileno,data).subscribe((response) => {
-      console.log(response);
-      console.log("Advance Added to database");
+    this.api.postApi("api/advance/"+mobileno,data).subscribe((response) => {
+      
     });
 }
 }
--- src/app/components/scheme/scheme.component.ts
+++ src/app/components/scheme/scheme.component.ts
@@ -17,20 +17,15 @@ export class SchemeComponent {
   
   ngOnInit() {
     this.api.getApi("api/schemes/").subscribe((response) => {
-      // this.allschemes = response;
+      console.log(response);
+      this.allschemes = response;
     });
-    //  Mocking the values since API not completed yet
-    this.allschemes=`[
-      {"Date": 1,"Description": "delectus aut autem","Amount": 12345},
-      {"Date": 2,"Description": "lorem","Amount": 2393209}
-    ]`;
-    this.allschemes=JSON.parse(this.allschemes);
-    console.log(this.allschemes)
   }
   add_scheme(mobileno:any){
       let data = {
-        Amount: this.schemeamt,
+        SchemeAmount: this.schemeamt,
         SchemeName: this.schemename,
+        SchemeDesc: this.desc,
       };
       this.api.postApi("api/schemes/"+mobileno,data).subscribe((response) => {
         console.log(response);
